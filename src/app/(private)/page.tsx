25176e193f75cd78e600d3a6ff7aff8083d1b04d
jeperico <joaoeperico@gmail.com>
2026-02-10 19:45:39 -0300

    feat: admin page

--- src/app/(private)/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Home() {
-  return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
-}
--- src/context/providers.tsx
+++ src/context/providers.tsx
@@ -1,9 +1,6 @@
 'use client';
 
 import React from 'react';
-import { usePathname } from 'next/navigation';
-import Header from '@/layout/header';
-import Footer from '@/layout/footer';
 import { Toaster } from '@/components/ui/sonner';
 
 interface IProvidersProps {
@@ -11,19 +8,9 @@ interface IProvidersProps {
 }
 
 const Providers: React.FC<IProvidersProps> = ({ children }) => {
-  const pathname = usePathname();
-
   return (
     <>
-      {pathname === '/login' ? (
-        <>{children}</>
-      ) : (
-        <>
-          <Header />
-          {children}
-          <Footer />
-        </>
-      )}
+      <>{children}</>
       <Toaster />
     </>
   );
